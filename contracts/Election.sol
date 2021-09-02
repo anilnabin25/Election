eb347ec84b10fe7277036df864def0840c32bce8
Divya Shrestha <53654225+anilnabin25@users.noreply.github.com>
2021-09-02 17:45:23 +0545

    adding the inital commit

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+//pragma solidity ^0.4.2;
+pragma solidity ^0.5.0;
+
+contract Election {
+  // Store Candidate
+  string  public candidate;
+
+  //  // Read and write candidate
+  //  string public candidate;
+  constructor() public {
+    candidate = "Candidate 1";
+  }
+
+}
